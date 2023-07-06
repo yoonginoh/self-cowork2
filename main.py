cc38762119afda7647bf33971ef7a006267e7f7e
DESKTOP-AEBPAVF\202-1576 <ksjnote@naver.com>
2023-07-06 12:00:07 +0900

    한번 더 하는 기능 추가

--- main.py
+++ main.py
@@ -6,3 +6,10 @@ for i in range(5) :
     lotto = random.sample(range(1,45),6)
     print(lotto)
 
+y = input("한번 더 추첨하려면 y를 입력하세요:")
+
+if y == "y" :
+    lotto = random.sample(range(1,45),6)
+    print(lotto)
+
+
